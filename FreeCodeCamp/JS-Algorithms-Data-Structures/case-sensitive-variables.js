4e8a728a6067f135675813592af965db749486fd
Jon McKay <jmckay@omni-guide.com>
2019-01-10 10:36:26 -0700

    Javascript variables are case sensitive

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/case-sensitive-variables.js
@@ -0,0 +1,11 @@
+// Javascript has case sensitive variables. The variables below have been changed to camelCase in order for the assignments to line up
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
