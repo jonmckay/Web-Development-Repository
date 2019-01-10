da8198589e1628692a16277def484934dae74c78
Jon McKay <jmckay@omni-guide.com>
2019-01-10 12:59:37 -0700

    Set a variable using the remainder operator

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/remainder-operator.js
@@ -0,0 +1,7 @@
+// The remainder operator in javascript is not the same as the modulus operator since it doesn't always give accurate results with negative numbers.
+
+// Only change code below this line
+
+var remainder;
+
+remainder = 11 % 3;
