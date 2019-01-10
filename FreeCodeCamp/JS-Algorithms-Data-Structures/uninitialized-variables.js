6e158c5128ddf7d94fd2a414cbbaecbf1692bde3
Jon McKay <jmckay@omni-guide.com>
2019-01-10 10:20:35 -0700

    Show basic javascript variable initialization

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/uninitialized-variables.js
@@ -0,0 +1,12 @@
+// Using an uninitialized variable will result in NaN. The variables below have been initialized (5, 10, "I am a") in order to prevent this error.
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
