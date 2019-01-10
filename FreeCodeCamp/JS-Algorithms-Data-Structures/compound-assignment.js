cbf7d7a0c53d3f008df673aa5504d736118e3e05
Jon McKay <jmckay@omni-guide.com>
2019-01-10 13:30:15 -0700

    Examples of compound assignments & escape sequences

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/compound-assignment.js
@@ -0,0 +1,46 @@
+// Use a compound assignment operator for the following expressions.
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+// Old:
+a = a + 12;
+b = b + 9;
+c = c + 7;
+
+// New:
+a += 12;
+b += 9;
+c += 7;
+
+// Old:
+a = a - 6;
+b = b - 15;
+c = c - 1;
+
+// New:
+a -= 6;
+b -= 15;
+c -= 1;
+
+// var a = 5;
+// var b = 12;
+// var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= b;
+c *= 10;
+
+// var a = 48;
+// var b = 108;
+// var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
