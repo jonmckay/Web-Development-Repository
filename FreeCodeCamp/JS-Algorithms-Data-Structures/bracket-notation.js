4ae44d67a69c36660d4dcc4b42366be6d47eae16
Jon McKay <jmckay0411@gmail.com>
2019-01-10 15:05:41 -0700

    Get the first char of string with bracket notation

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/bracket-notation.js
@@ -0,0 +1,15 @@
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName;
+
+// Uses bracket notation to get the first letter of the last name
+firstLetterOfLastName = lastName[0];
