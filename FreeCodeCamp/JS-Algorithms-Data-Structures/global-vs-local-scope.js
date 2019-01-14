e19352ba57386e9ee094ba1c98168a3f24eda268
Jon McKay <jmckay0411@gmail.com>
2019-01-14 15:09:22 -0500

    Compare local and global scoped vars with the same name

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/global-vs-local-scope.js
@@ -0,0 +1,16 @@
+// When defining both a global and local scoped variable, the local definition takes precedence.
+
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+  
+  
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
