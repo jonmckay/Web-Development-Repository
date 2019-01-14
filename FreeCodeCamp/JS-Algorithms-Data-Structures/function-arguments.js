e50f88445b390ee6bac76e6a5c1da12175677568
Jon McKay <jmckay0411@gmail.com>
2019-01-14 13:23:04 -0500

    Create a function with arguments that adds them up

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/function-arguments.js
@@ -0,0 +1,15 @@
+// Create a function with two arguments and add them together. Pass the function two numbers.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+    console.log(a - b);
+  }
+  ourFunctionWithArgs(10, 5); // Outputs 5
+  
+  // Only change code below this line.
+  function functionWithArgs(num, num2) {
+    console.log(num + num2);
+  }
+  
+  functionWithArgs(1, 2);
+  
