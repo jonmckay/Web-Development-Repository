296f61b9a63d62f58cce13d8bfe39de0d3521ab5
Jon McKay <jmckay0411@gmail.com>
2019-01-22 19:54:04 -0700

    Remove items from an array with pop

--- FreeCodeCamp/JS-Algorithms-Data-Structures/global-vs-local-scope.js
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/global-vs-local-scope.js
@@ -5,12 +5,12 @@
 var outerWear = "T-Shirt";
 
 function myOutfit() {
-  // Only change code below this line
-  var outerWear = "sweater";
+	// Only change code below this line
+	var outerWear = "sweater";
   
   
-  // Only change code above this line
-  return outerWear;
+	// Only change code above this line
+	return outerWear;
 }
 
 myOutfit();
