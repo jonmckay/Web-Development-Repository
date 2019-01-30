53a2b2ad1117f3e3a668ed55b2bc6a7e1e128c62
Jon McKay <jmckay0411@gmail.com>
2019-01-29 18:46:22 -0700

    Basic example of 'else' statement

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/Basic-JavaScript/else-statements.js
@@ -0,0 +1,19 @@
+// Shows example of combining two if statements into an if/else statement.
+
+function testElse(val) {
+    var result = "";
+    // Only change code below this line
+    
+    if (val > 5) {
+      result = "Bigger than 5";
+    } else {
+      result = "5 or Smaller";
+    }
+    
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  testElse(4);
+  
