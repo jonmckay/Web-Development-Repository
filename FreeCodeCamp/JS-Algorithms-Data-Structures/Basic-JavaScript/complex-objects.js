5b9106bb9c2998cf83fd45db418f58c0ac63c188
Jon McKay <jmckay0411@gmail.com>
2019-02-04 20:15:47 -0700

    Add a complex object to an array JSON Object

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/Basic-JavaScript/complex-objects.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Artist Name",
+    "title": "song name",
+    "release_year": 2019,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  }
+];
