0dd61e9cd0db4618d02adab6be8fc12e8c82fb52
Jon McKay <jmckay0411@gmail.com>
2019-02-12 19:42:00 -0700

    Use bracket and dot notation to access nested object properties

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/Basic-JavaScript/nested-objects.js
@@ -0,0 +1,16 @@
+// Use dot and bracket notation to access the contents of the glove box and assign them to gloveBoxContents
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
