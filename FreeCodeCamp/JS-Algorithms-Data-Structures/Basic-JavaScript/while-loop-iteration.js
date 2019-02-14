3556a6be84136d766173f3fa4bd3f5a4a1d34d9f
Jon McKay <jmckay0411@gmail.com>
2019-02-13 21:28:53 -0700

    Iterate through an array using a while loop

---
+++ FreeCodeCamp/JS-Algorithms-Data-Structures/Basic-JavaScript/while-loop-iteration.js
@@ -0,0 +1,11 @@
+// Iterate using a while loop and push 0 to 4 to myArray
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+    myArray.push(i);
+    i++;
+}
